c0b9e5d8f326eaa1da0323bb7d3e4c04dc60aba4
Daniel Buscaglia <danbuscaglia@gmail.com>
2019-10-22 16:51:54 -0700

    Fixing dependencies.

---
+++ design-system/src/DesignSystem.ts
@@ -0,0 +1,9 @@
+import {palette, spacing, sizing, borderRadius, typography} from './constants';
+
+export class DesignSystem {
+  palette = palette;
+  spacing = spacing;
+  sizing = sizing;
+  borderRadius = borderRadius;
+  typography = typography;
+}
