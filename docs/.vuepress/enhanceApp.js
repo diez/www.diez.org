cd5610beb0bfed4435086ffec8cf20b7bdb2a0d6
Roberto Dip <dip.jesusr@gmail.com>
2019-10-23 19:43:23 -0300

    tweak: use custom dimension name

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -12,7 +12,9 @@ export default ({router}) => {
   router.afterEach(() => {
     if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
       // window.currentBranch is set by Netlify via the 'snippet injection' functionality
-      ga('set', 'experiment-id', window.currentBranch);
+      // dimension1 maps to 'experiment-id' in GA, you can edit or add more dimensions via the
+      // GA interface.
+      ga('set', 'dimension1', window.currentBranch);
     }
   });
 };
